ca33da8e18be1c0958e541e087a6689569efecd7
Oscar <oscar_ochoac@yahoo.com>
2019-07-17 03:14:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -60,50 +60,56 @@ def apply_clearance(cart)
 
 end
 
+
+# The `checkout` method will need to utilize all the previous methods we've
+# written. It consolidates the cart, applies coupons, and applies discounts. Then,
+# it totals the cost of the entire cart, accounting for each item and their
+# prices, and returns this value.
+
+# When writing this method, make sure to address each step in the proper order:
+
+# - Consolidate the cart array into a hash
+
+# - Apply coupon discounts if the proper number of items are present
+
+# - Apply 20% discount if items are on clearance
+
+# In addition to coupons and clearance, our grocer store offers a deal for
+# customers buying lots of items: if, after all coupons and discounts, the cart's
+# total is over $100, the customer gets an additional 10% off. Apply this
+# discount when appropriate.
 def checkout(cart, coupons)
   
   total = 0
   
-  consolidated = consolidate_cart(cart)
+  cart = consolidate_cart(cart)
   
   if consolidated.length == 1
-    apply_coupons(consolidated, coupons)
+    cart = apply_coupons(consolidated, coupons)
+    cart_clearance = apply_clearance(cart)
     
-    apply_clearance(cart)
-    
+    if cart_clearance.length > 1
+      cart_clearance.each do |items, details|
+        if details[:count] >=1
+          total += (details[:price]*details[:count])
+        end
+      end
+    else
+      cart_clearance.each do |item, details|
+        total += (details[:price]*details[:count])
+      end
+    end
   else
-    
+    cart = apply_coupons(cart, coupons)
+    cart_clearance = apply_clearance(cart)
+    cart_clearance.each do |item, details|
+      total += (details[:price]*details[:count])
+    end
   end
   
-  
-  total
-  
-  
-  #   if cart.length == 1
-  #   cart = apply_coupons(cart, coupons)
-  #   cart_clearance = apply_clearance(cart)
-  #   if cart_clearance.length > 1
-  #     cart_clearance.each do |item, details|
-  #       if details[:count] >=1
-  #         total += (details[:price]*details[:count])
-  #       end
-  #     end
-  #   else
-  #     cart_clearance.each do |item, details|
-  #       total += (details[:price]*details[:count])
-  #     end
-  #   end
-  # else
-  #   cart = apply_coupons(cart, coupons)
-  #   cart_clearance = apply_clearance(cart)
-  #   cart_clearance.each do |item, details|
-  #     total += (details[:price]*details[:count])
-  #   end
-  # end
-  
 
-  # if total > 100
-  #   total = total*(0.90)
-  # end
-  # total
+  if total > 100
+    total = total*(0.90)
+  end
+  total
 end
